8f73ad6f53edf6efdeb6e8eec8622e8bad29c986
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-02-19 17:51:38 -0600

    updated with 2 implementation versions of poisson on hist

--- time.py
+++ time.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 import matplotlib.pylab as plt
+from scipy.stats import norm
 from scipy.stats import poisson
 from scipy.optimize import curve_fit
+from scipy.special import gammaln # x! = Gamma(x+1)
 from scipy.special import factorial
 from matplotlib.ticker import FormatStrFormatter
 
@@ -58,58 +60,72 @@ def param_default():
 
 param_default()
 
-
-
+# Main data array
 time=[254, 11, 4, 3.5, 1.8, 3.6, 4.5, 4.3, 59, 17, 338, 40, 6.85, 58] # time to failure data
 
 
+# Below are the 2 implementations of poisson/histogram fit generation
 
 
-entries, bin_edges, patches = plt.hist(x=time, bins='auto', color='#0504aa',
-                            alpha=0.7, rwidth=0.85) #bin -> x asis time window, n-> number of failure in y-axis
-print(entries, bin_edges)
-plt.grid(axis='y', alpha=0.75)
-plt.xlabel('Time  to System Failure (Mins)')
-plt.ylabel('Frequency (#)')
-plt.text(130, 5, r'$\mu$P' + ' irradiated with '+ r'0.1 $\mu$'+'Ci source') #insert text in fig
-plt.savefig('time.png')
-plt.savefig('time.pdf')
+# ***************************************************************************************************************************
+# VERSION 1
+# ***************************************************************************************************************************
+#
+# This is the best version I have been able to come up with. The plot uses the original x-axis but normalized the y-axis
+# or frequency axis to scale from 0-0.1, rather than 0-10 (this can be changed if desired)
+#
+# This implementation also includes plots of both normal and poisson distributions on the data given in the histogram.
+# This is the best fit I have been able to achieve thus far
 
+# Perform jacobian transformation computation for poisson
+def transformation_and_jacobian(x):
+    return 1./x, 1./x**2.
 
-# # the bins should be of integer width, because poisson is an integer distribution
-# bins = np.arange(11) - 0.5
-# entries, bin_edges, patches = plt.hist(time, bins=bins, density=True, label='Data')
+# Generate normal distribution from data
+def tfm_normal_pdf(x, lam):
+    y, J = transformation_and_jacobian(x)
+    return norm.pdf(y, lam, np.sqrt(lam)) * J
 
-# calculate bin centres
-bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+# Generate poisson distribution from data
+def tfm_poisson_pdf(x, mu):
+    y, J = transformation_and_jacobian(x)
+    return np.exp(y * np.log(mu) - mu - gammaln(y + 1.)) * J
 
+# Call histogram plot on data
+hist, bins = np.histogram(time, bins=50, density=True)
 
-def fit_function(k, lamb):
-    '''poisson function, parameter lamb is the fit parameter'''
-    return poisson.pmf(k, lamb)
+# Generate width and center values, normalize dataset as needed for normal dist
+# Note that this normalization is done for the normal distribution: if only poisson is desired,
+# you can remove this
+width = 0.8*(bins[1]-bins[0])
+center = (bins[:-1]+bins[1:])/2
+plt.bar(center, hist, align='center', width=width, label = 'Normalised data')
 
+# Choose starting point for normal dist
+p0 = 1 / np.mean(time)
 
-# fit with curve_fit
-parameters, cov_matrix = curve_fit(fit_function, bin_middles, entries)
+# Fit the fitted versions of the normal and poisson distributions onto the histogram
+norm_opt, _ = curve_fit(tfm_normal_pdf, center, hist, p0=p0)
+pois_opt, _ = curve_fit(tfm_poisson_pdf, center, hist, p0=p0)
 
-# plot poisson-deviation with fitted parameter
-x_plot = np.arange(0, 350)
+# Plot everything together
+plt.plot(center, tfm_normal_pdf(center, *norm_opt), 'g--', label='Normal Fit (post-transformation)')
+plt.plot(center, tfm_poisson_pdf(center, *pois_opt), 'r--', label='Poisson Fit (post-transformation)')
 
-plt.plot(
-    x_plot,
-    fit_function(x_plot, *parameters),
-    marker='o', linestyle='',
-    label='Fit result',
-)
 plt.legend()
 plt.show()
 
-
+# ***************************************************************************************************************************
+# VERSION 2
+# ***************************************************************************************************************************
+#
+# This is a poisson attempt on the original plot of data. The only things plotted is the histogram
+# and the originally given histogram. The y-axis is on a scale of 0-10 for frequency
 
 #
-# n, bins, patches = plt.hist(x=time, bins='auto', color='#0504aa',
+# entries, bin_edges, patches = plt.hist(x=time, bins='auto', color='#0504aa',
 #                             alpha=0.7, rwidth=0.85) #bin -> x asis time window, n-> number of failure in y-axis
-# print(n, bins)
+# print(entries, bin_edges)
 # plt.grid(axis='y', alpha=0.75)
 # plt.xlabel('Time  to System Failure (Mins)')
 # plt.ylabel('Frequency (#)')
@@ -117,19 +133,39 @@ plt.show()
 # plt.savefig('time.png')
 # plt.savefig('time.pdf')
 #
-# #Trial Poisson Fit
-# def fit_function(k, lambda_val):
-#     return poisson.pmf(k, lambda_val)
 #
-# # x_rvs=pd.Series(poisson.rvs(22, size=100000, random_state=2))
-# # data=x_rvs.value_counts().sort_index().to_dict()
-# # fig, ax= plt.subplots()
-# # ax.bar(range(len(data)), list(data.values()), align='center')
-# # plt.xticks(range(len(data)), list(data.keys()))
+# # # the bins should be of integer width, because poisson is an integer distribution
+# # bins = np.arange(11) - 0.5
+# # entries, bin_edges, patches = plt.hist(time, bins=bins, density=True, label='Data')
 #
-# # t = np.arange(0, 5, 0.1)
-# # x = np.power(t, 5)
-# # y = factorial(t)
-# #
-# # plt.plot( t, x / y, 'bs')
+# # calculate bin centres
+# bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+#
+#
+# def fit_function(k, lamb):
+#     '''poisson function, parameter lamb is the fit parameter'''
+#     return poisson.pmf(k, lamb)
+#
+# def transformation_and_jacobian(x):
+#     return 1./x, 1./x**2.
+#
+# def tfm_poisson_pdf(x, mu):
+#     y, J = transformation_and_jacobian(x)
+#     # For numerical stability, compute exp(log(f(x)))
+#     return np.exp(y * np.log(mu) - mu - gammaln(y + 1.)) * J
+#
+#
+# # fit with curve_fit
+# parameters, cov_matrix = curve_fit(fit_function, bin_middles, entries)
+#
+# # plot poisson-deviation with fitted parameter
+# x_plot = np.arange(0, 350)
+#
+# plt.plot(
+#     x_plot,
+#     fit_function(x_plot, *parameters),
+#     marker='o', linestyle='',
+#     label='Fit result',
+# )
+# plt.legend()
 # plt.show()
